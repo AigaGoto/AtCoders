a2059dd0753508f18dedae4b16c19b88dea9480f
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-06-24 11:43:18 +0900

    027 complete

--- Selected90/027_SignUpRequests.cpp
+++ Selected90/027_SignUpRequests.cpp
@@ -9,10 +9,23 @@
 #include <iomanip>
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for (int i = 1; i <= (int)(n); i++)
+
+int N;
+map<string, int> Map;
 
 int main() {
+    cin >> N;
 
+    string S;
+
+    rep(i, N) {
+        cin >> S;
+        if (Map[S] == 0) {
+            cout << i << endl;
+            Map[S] = 1;
+        }
+    }
 
     return 0;
 }
