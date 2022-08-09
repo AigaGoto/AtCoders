40eb68392042ff21f06706c63e2f91ac981199b9
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-08-09 15:17:45 +0900

    complete 034 しゃくとりほう

--- Selected90/034_ThereAreFewTypesOfElements.cpp
+++ Selected90/034_ThereAreFewTypesOfElements.cpp
@@ -14,11 +14,29 @@ using namespace std;
 int N, K;
 long a[100009];
 
+map<int, int> typeMap;
+
 int main() {
     cin >> N >> K;
     rep(i, N) cin >> a[i];
 
-    
+    int right = 0;
+    int type_num = 0;
+    int max_length = 0;
+
+    for(int left = 0; left < N; left++) {
+        while(right < N) {
+            if (typeMap[a[right]] == 0 && type_num == K) break;
+            if (typeMap[a[right]] == 0) type_num++;
+            typeMap[a[right]] += 1;
+            right++;
+        }
+        max_length = max(max_length, right - left);
+        if (typeMap[a[left]] == 1) type_num--;
+        typeMap[a[left]] -= 1;
+    }
+
+    cout << max_length << endl;
 
     return 0;
 }
