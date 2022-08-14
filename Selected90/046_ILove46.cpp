9f2e25d554aa885e3c09be65394a55dfc385348c
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-08-14 10:32:56 +0900

    complete 46

--- Selected90/046_ILove46.cpp
+++ Selected90/046_ILove46.cpp
@@ -11,8 +11,39 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-int main() {
+int N;
+long x;
+vector<int> am(46),bm(46),cm(46);
 
+int main() {
+    cin >> N;
+
+    rep(i, N) {
+        cin >> x;
+        am[x % 46]++;
+    }
+    rep(i, N) {
+        cin >> x;
+        bm[x % 46]++;
+    }
+    rep(i, N) {
+        cin >> x;
+        cm[x % 46]++;
+    }
+
+    long long all_count = 0;
+
+    rep(i, 46) {
+        rep(j, 46) {
+            rep(k, 46) {
+                if ((i + j + k) % 46 == 0) {
+                    all_count += (long long )(am[i]) * bm[j] * cm[k]; 
+                }
+            }
+        }
+    }
+
+    cout << all_count << endl;
 
     return 0;
 }
