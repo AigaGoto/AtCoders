eb33a3c7e85210056c50caeea446b1973aede285
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-08-16 09:26:33 +0900

    complete 48

--- Selected90/048_IWillNotDropOut.cpp
+++ Selected90/048_IWillNotDropOut.cpp
@@ -1,3 +1,5 @@
+// 部分点が半分以上であることを利用
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,8 +13,30 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-int main() {
+int N, K;
+vector<long> A,B,C;
+long inp;
 
+int main() {
+    cin >> N >> K;
+    rep(i, N) {
+        cin >> inp;
+        A.push_back(inp);
+        cin >> inp;
+        B.push_back(inp);
+        C.push_back(B[i]);
+        C.push_back(A[i] - B[i]);
+    }
+
+    sort(C.begin(), C.end(),greater<int>());
+
+    long long sum = 0;
+
+    rep(i, K) {
+        sum += C[i];
+    }
+
+    cout << sum << endl;
 
     return 0;
 }
