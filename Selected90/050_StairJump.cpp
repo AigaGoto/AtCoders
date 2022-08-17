b982c0492d4186045baa2061be025027d78e7581
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-08-17 13:08:40 +0900

    050 complete

--- Selected90/050_StairJump.cpp
+++ Selected90/050_StairJump.cpp
@@ -1,3 +1,5 @@
+// 漸化式を立ててDPを解く
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,8 +13,25 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-int main() {
+const int divide = 1e9+7;
 
+int N, L;
+long long DP[100009];
+
+int main() {
+    cin >> N >> L;
+
+    DP[0] = 1;
+
+    rep(i, N+1) {
+        if (i == 0) DP[i] = 1;
+        else if (0 < i && i < L) DP[i] = DP[i-1];
+        else {
+            DP[i] = (DP[i-1] + DP[i-L]) % divide;
+        }
+    }
+
+    cout << DP[N] << endl;
 
     return 0;
 }
